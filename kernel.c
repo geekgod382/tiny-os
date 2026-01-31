39c1d172e0a02ff4785efd1cf660a066c066a1b6
Manan Sharma <manan140306@gmail.com>
2026-01-31 16:47:53 +0530

    working on filesystem

--- kernel.c
+++ kernel.c
@@ -1,9 +1,34 @@
 #include <stdint.h>
 #include "io.h"
+#include "fs.h"
 
 static volatile uint16_t* const VGA_BUFFER = (uint16_t*)0xB8000;
 static const int VGA_COLS = 80;
 static const int VGA_ROWS = 25;
+
+// VGA text mode output
+static uint8_t vga_entry_color(uint8_t fg, uint8_t bg){
+    return fg | (bg << 4);
+}
+
+static uint16_t vga_entry(char c, uint8_t color){
+    return (uint16_t)c | (uint16_t)(color << 8);
+}
+
+static void clear_screen(uint8_t color){
+    for (int i = 0; i < VGA_COLS * VGA_ROWS; ++i){
+        VGA_BUFFER[i] = vga_entry(' ', color);
+    }
+}
+
+static void kprint_at(const char* msg, int row, int col, uint8_t color){
+    int index = row * VGA_COLS + col;
+    for (int i = 0; msg[i] != '\0'; ++i){
+        VGA_BUFFER[index + i] = vga_entry(msg[i], color);
+    }
+}
+
+
 
 // Cursor control functions
 void enable_cursor(uint8_t cursor_start, uint8_t cursor_end) {
@@ -28,28 +53,6 @@ void update_cursor(int row, int col) {
     outb(0x3D5, (uint8_t)((pos >> 8) & 0xFF));
 }
 
-
-// VGA text mode output
-static uint8_t vga_entry_color(uint8_t fg, uint8_t bg){
-    return fg | (bg << 4);
-}
-
-static uint16_t vga_entry(char c, uint8_t color){
-    return (uint16_t)c | (uint16_t)(color << 8);
-}
-
-static void clear_screen(uint8_t color){
-    for (int i = 0; i < VGA_COLS * VGA_ROWS; ++i){
-        VGA_BUFFER[i] = vga_entry(' ', color);
-    }
-}
-
-static void kprint_at(const char* msg, int row, int col, uint8_t color){
-    int index = row * VGA_COLS + col;
-    for (int i = 0; msg[i] != '\0'; ++i){
-        VGA_BUFFER[index + i] = vga_entry(msg[i], color);
-    }
-}
 
 // Notepad implementation
 
@@ -273,8 +276,9 @@ void kernel_main(void){
     // Should not reach here if ISR halts
     // kprint_at("ERROR: Returned from INT 0!", 15, 2, vga_entry_color(15, 4));
 
+    fs_init();
     main_menu();
-    
+
     // for (;;){
     //     __asm__ __volatile__("hlt");
     // }
